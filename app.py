3a982c265b98017f9977550132822a7bf38146d8
Abhijendra <abhijendra7@gmail.com>
2025-03-30 10:56:28 +0530

    Add streamlit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import javalang
 import ollama
+import streamlit as st
 
 
-def read_java_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
+def read_java_file(uploaded_file):
+    java_code = uploaded_file.read().decode('utf-8')  # Decode bytes to string
+    return java_code
     
 
 def parse_java_code(code):
@@ -45,16 +46,31 @@ def generate_documentation(class_info):
                 {class_info['name']}
                 Explain it in a way that a beginner can understand."""
                 
-    response = ollama.chat(model="mistral",
+    response = ollama.chat(model="gemma:2b",
                            messages= [{"role": "user", "content": prompt}])
     
     return response['message']['content']
 
-    
-    
+
+
 
 if __name__ == '__main__':
     print("Setting up things...")
-    java_code = read_java_file('Car.java')
-    class_info = parse_java_code(java_code)
-    documentation = generate_documentation(class_info)
+    
+    #java_code = read_java_file('Car.java')
+    #class_info = parse_java_code(java_code)
+    # documentation = generate_documentation(class_info)
+    
+    st.title('Java Doc Generator')
+    uploaded_file = st.file_uploader("Upload a java file", type=["java"])
+    
+    if uploaded_file is not None:
+        java_code = read_java_file(uploaded_file)
+        class_info = parse_java_code(java_code)
+        
+        if st.button("Generate Documentation"):
+            with st.spinner("Generating documentation..."):
+                print("Generating documentation...")
+                documentation = generate_documentation(class_info)
+            st.success("Documentation generated successfully!")
+            st.text_area("Generated Documentation", documentation, height=300)    
